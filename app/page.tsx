710f822e560c9536f7b81e3f0ef954c456d4d365
Harith Yerragolam <yharith16@gmail.com>
2025-02-28 19:09:49 +0530

    Add initial Home component with basic structure

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Home() {
+return (
+    <main className="overflow-hidden">
+
+    </main>
+);
+}
