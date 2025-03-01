258cc781faf81bff732870c11f2d3cf86cc7ebe7
Harith Yerragolam <yharith16@gmail.com>
2025-03-01 19:08:47 +0530

    Update ESLint configuration and adjust SliderCSS imports for better performance
    Fixed Build Issues

--- app/components/layout/SliderCSS.tsx
+++ app/components/layout/SliderCSS.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { useEffect } from 'react';
 
 const SliderCSS = () => {
   useEffect(() => {
-    import('slick-carousel/slick/slick.css');
-    import('slick-carousel/slick/slick-theme.css');
+    // import('slick-carousel/slick/slick.css');
+    // import('slick-carousel/slick/slick-theme.css');
   }, []);
   
   return null;
