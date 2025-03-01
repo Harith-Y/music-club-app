a1ecbb8aa121367598cc9e5a3621a490225916f9
Harith Yerragolam <yharith16@gmail.com>
2025-03-01 17:50:26 +0530

    Add EventsSection component with upcoming events display and styling updates

--- app/components/sections/EventsSection.tsx
+++ app/components/sections/EventsSection.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 
 import AnimatedSection from '../layout/AnimatedSection';
@@ -7,7 +9,19 @@ import { events } from '../../data/events';
 const EventsSection = () => {
   return (
     <AnimatedSection id="events">
-        
+      <div className="container mx-auto px-4 md:px-6">
+        <h2 className="section-heading">Upcoming Events</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-12">
+          {events.map((event) => (
+            <EventCard key={event.id} event={event} />
+          ))}
+        </div>
+        <div className="text-center mt-10">
+          <Link href="#" className="btn-secondary">
+            View All Events
+          </Link>
+        </div>
+      </div>
     </AnimatedSection>
   );
 };
