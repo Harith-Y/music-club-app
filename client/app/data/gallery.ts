c371460bee422c774f2fcb82dffc622246e12e06
Harith Yerragolam <yharith16@gmail.com>
2025-04-19 02:06:48 +0530

    Add new competition entries and images to the gallery
    - Expanded the GalleryItem interface with additional competition entries for the Music Reel Challenge.
    - Included new image files for participants: Benedict, Caitlin, Duo, Mayukha, Ranjan, Saniya, Shashwat Singh, Suranjana, Tarun, and Vijay.
    - Enhanced the gallery's content to showcase a wider range of participants and their contributions.

--- client/app/data/gallery.ts
+++ client/app/data/gallery.ts
@@ -17,5 +17,15 @@ export interface GalleryItem {
 
     { id: 6, category: 'jams', image: '/gallery/OpenMicNight-August 31/20240831_201107.jpg', title: 'Anuj and Anushree', type: 'image', event: '1st Open Mic Night' },
     { id: 7, category: 'competitions', image: '/gallery/MusicReelChallenge/Harini.jpg', title: 'Harini', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/t46qLSQgBUY' },
-
+    { id: 8, category: 'competitions', image: '/gallery/MusicReelChallenge/Suranjana.jpg', title: 'Suranjana Mary Paul Santiago', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/vv6BadAlzVs' },
+    { id: 9, category: 'competitions', image: '/gallery/MusicReelChallenge/Vijay.jpg', title: 'Vijay', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/31NE44FVR8g' },
+    { id: 10, category: 'competitions', image: '/gallery/MusicReelChallenge/Caitlin.jpg', title: 'Caitlin', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/DUq0kznKRwU' },
+    { id: 11, category: 'competitions', image: '/gallery/MusicReelChallenge/Mayukha.jpg', title: 'Mayukha', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/_o3urkRycMg' },
+    { id: 12, category: 'competitions', image: '/gallery/MusicReelChallenge/Ranjan.jpg', title: 'Ranjan', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/zBUYhD8EIyA' },
+    { id: 13, category: 'competitions', image: '/gallery/MusicReelChallenge/Benedict.jpg', title: 'Benedict I', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/OuMlbzoIhs8' },
+    { id: 14, category: 'competitions', image: '/gallery/MusicReelChallenge/ShashwatSingh.jpg', title: 'Shashwat Singh', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/6LimLLpqOTE' },
+    { id: 15, category: 'competitions', image: '/gallery/MusicReelChallenge/Saniya.jpg', title: 'Katkam Saniya', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/4iDT6WU6MtI' },
+    { id: 16, category: 'competitions', image: '/gallery/MusicReelChallenge/Duo.jpg', title: 'Anurag and Grishmank', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/nHTZT4Ky9qU' },
+    { id: 17, category: 'competitions', image: '/gallery/MusicReelChallenge/Tarun.jpg', title: 'Tarun Mamillapalli', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/woGKn0s-mhc' },
+    { id: 18, category: 'competitions', image: '/gallery/MusicReelChallenge/Benedict2.jpg', title: 'Benedict I', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/4D2zzB3mAVw' },
   ];
