51be318fb8b41079fdb45bcda123d6e989a6963b
Harith-Y <144448206+Harith-Y@users.noreply.github.com>
2025-04-18 11:29:30 +0530

    Update gallery.ts
    Add Room 823 Farewell band videos

--- client/app/data/gallery.ts
+++ client/app/data/gallery.ts
@@ -13,5 +13,5 @@ export interface GalleryItem {
     { id: 3, category: 'performances', image: '/performances/IMG_9793.jpg', title: 'Blastroduction', type: 'image' },
     { id: 4, category: 'jams', image: '/performances/20240831_210858.jpg', title: 'Weekend Jam Session', type: 'image' },
     { id: 5, category: 'covers', image: '/gallery/Enter_Sandman.png', title: 'Enter Sandman', type: 'video', videoUrl: 'https://www.youtube.com/embed/hpURyv_xhu0' },
-    // { id: 6, category: 'workshops', image: '/gallery/guitar-masterclass.jpg', title: 'Guitar Masterclass', type: 'video', videoUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ' }
+    { id: 6, category: 'covers', image: '/gallery/Room-823_Farewell.jpg', title: 'Hua Mein', type: 'video', videoUrl: 'https://youtube.com/embed/RtaddCi6TFo' }
   ];
