c8cfc17b7082ed95ca35284cd38b3f35711326a0
Harith Yerragolam <yharith16@gmail.com>
2025-04-20 00:13:26 +0530

    Add new video entry for 'Dream On' to the gallery, enhancing the collection of performance content.

--- client/app/data/gallery.ts
+++ client/app/data/gallery.ts
@@ -14,6 +14,7 @@ export interface GalleryItem {
     { id: 3, category: 'performances', image: '/performances/IMG_9793.jpg', title: 'Blastroduction', type: 'image' },
     { id: 4, category: 'jams', image: '/performances/20240831_210858.jpg', title: 'Weekend Jam Session', type: 'image' },
     { id: 5, category: 'performances', image: '/gallery/Enter_Sandman.png', title: 'Enter Sandman', type: 'video', videoUrl: 'https://www.youtube.com/embed/hpURyv_xhu0' },
+    { id: 6, category: 'performances', image: '/gallery/Dream_On.png', title: 'Dream On', type: 'video', videoUrl: 'https://www.youtube.com/embed/_S_q-sEy6Pg' },
 
     { id: 6, category: 'jams', image: '/gallery/OpenMicNight-August 31/20240831_201107.jpg', title: 'Anuj and Anushree', type: 'image', event: '1st Open Mic Night' },
     { id: 7, category: 'competitions', image: '/gallery/MusicReelChallenge/Harini.jpg', title: 'Harini', type: 'video', event: 'Music Reel Challenge', videoUrl: 'https://youtube.com/embed/t46qLSQgBUY' },
